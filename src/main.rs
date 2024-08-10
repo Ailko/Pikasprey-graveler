f811efd4ff365d989f215a58b9aaecfc3478e370
Ailko Claeys <ailko.claeys@outlook.com>
2024-08-10 19:55:27 +0200

    Add in a timer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::stdin;
+use std::{
+    io::stdin,
+    time::Instant
+};
 use rand::{thread_rng, Rng};
 
 fn main() {
@@ -13,6 +16,7 @@ fn main() {
 
     let mut most_paralyzed_turns: u8 = 0;
 
+    let start_time = Instant::now();
     while played_battles < battles && most_paralyzed_turns < 177 {
         let mut paralyzed: u8 = 0;
         for _ in 0..231 {
@@ -25,7 +29,9 @@ fn main() {
         }
         played_battles += 1;
     }
+    let duration = start_time.elapsed();
 
     println!("Highest amount of paralyzed turns: {most_paralyzed_turns}");
     println!("Number of battles simulated: {played_battles}");
+    println!("The code took: {}s", duration.as_secs_f64());
 }
