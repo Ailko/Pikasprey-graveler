cc1c164ad7c959e3df78766a83e721b353f7eabd
Ailko Claeys <ailko.claeys@outlook.com>
2024-08-11 22:08:12 +0200

    Reduced rng calls from 231 to 11

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,33 @@ use std::{
 use rand::{thread_rng, Rng};
 
 fn main() {
+    // Distribution of all possible outcomes for 21 turns of 4 options
+    fn rng_to_paralyzed_turns(rng: u64) -> u8 {
+        if rng < 10460353203 {return 0}
+        else if rng < 83682825624 {return 1}
+        else if rng < 327757733694 {return 2}
+        else if rng < 843026984064 {return 3}
+        else if rng < 1615930859619 {return 4}
+        else if rng < 2491888585248 {return 5}
+        else if rng < 3270517674696 {return 6}
+        else if rng < 3826681310016 {return 7}
+        else if rng < 4151110097286 {return 8}
+        else if rng < 4307316550416 {return 9}
+        else if rng < 4369799131668 {return 10}
+        else if rng < 4390626658752 {return 11}
+        else if rng < 4396412082942 {return 12}
+        else if rng < 4397747180832 {return 13}
+        else if rng < 4398001485192 {return 14}
+        else if rng < 4398041043648 {return 15}
+        else if rng < 4398045988455 {return 16}
+        else if rng < 4398046473240 {return 17}
+        else if rng < 4398046509150 {return 18}
+        else if rng < 4398046511040 {return 19}
+        else if rng < 4398046511103 {return 20}
+        else {return 21}
+
+    }
+
     fn run_simulations(battles: u32) -> (u8, u32) {
         let mut rng = thread_rng();
         let mut most_paralyzed: u8 = 0;
@@ -11,10 +38,8 @@ fn main() {
         for i in 1..battles + 1 {
             battles_played = i;
             let mut paralyzed_turns: u8 = 0;
-            for _ in 0..231 {
-                if rng.gen_range(0..4 as u8) == 0 {
-                    paralyzed_turns += 1;
-                }
+            for _ in 0..11 {
+                paralyzed_turns += rng_to_paralyzed_turns(rng.gen_range(0..4398046511104 as u64));
             }
             if paralyzed_turns > most_paralyzed {
                 most_paralyzed = paralyzed_turns;
