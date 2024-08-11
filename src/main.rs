73645e09c3ac3896befabc64019d7d06a8896047
Ailko Claeys <ailko.claeys@outlook.com>
2024-08-11 14:38:46 +0200

    Remove need for locks

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,64 @@
 use std::{
-    io::stdin, sync::{Arc, Mutex, MutexGuard}, thread::{self, JoinHandle}, time::{Duration, Instant}
+    io::stdin, thread::{self, JoinHandle}, time::{Duration, Instant}
 };
 use rand::{thread_rng, Rng};
 
 fn main() {
-    fn increment_played_battles(mut played_battles: MutexGuard<u32>, battles: u32) -> Option<()> {
-        if *played_battles >= battles {
-            return None;
+    fn run_simulations(battles: u32) -> (u8, u32) {
+        let mut rng = thread_rng();
+        let mut most_paralyzed: u8 = 0;
+        let mut battles_played: u32 = 0;
+        for i in 1..battles + 1 {
+            battles_played = i;
+            let mut paralyzed_turns: u8 = 0;
+            for _ in 0..231 {
+                if rng.gen_range(0..4 as u8) == 0 {
+                    paralyzed_turns += 1;
+                }
+            }
+            if paralyzed_turns > most_paralyzed {
+                most_paralyzed = paralyzed_turns;
+            }
+            if most_paralyzed >= 177 {
+                return (most_paralyzed, battles_played);
+            }
         }
-        *played_battles += 1;
-        return Some(())
-    }
-
-    fn update_most_paralyzed_turns(mut most_paralyzed_turns: MutexGuard<u8>, update_value: u8) {
-        if *most_paralyzed_turns >= update_value {
-            return;
-        }
-        *most_paralyzed_turns = update_value.clone();
+        return (most_paralyzed, battles_played);
     }
 
     println!("How many battles would you like to attempt?");
     let mut buf: String = "".to_owned();
     stdin().read_line(&mut buf).unwrap();
 
-    let battles = u32::from_str_radix(&buf.trim(), 10).unwrap();
-    let played_battles = Arc::new(Mutex::new(0));
-
-    let most_paralyzed_turns = Arc::new(Mutex::new(0));
+    let mut battles = u32::from_str_radix(&buf.trim(), 10).unwrap();
     
     let thread_count = thread::available_parallelism().unwrap();
-    let mut threads: Vec<JoinHandle<()>> = vec![];
+    let mut threads: Vec<JoinHandle<(u8, u32)>> = vec![];
 
     println!("Running with {thread_count} threads");
 
     let start_time: Instant = Instant::now();
 
-    for _ in 0..thread_count.into() {
-        let played_battles = Arc::clone(&played_battles);
-        let most_paralyzed_turns = Arc::clone(&most_paralyzed_turns);
-
-        let thread: JoinHandle<()> = thread::spawn(move || {
-            let mut rng = thread_rng();
-            while increment_played_battles(played_battles.lock().unwrap(), battles).is_some() && {*most_paralyzed_turns.lock().unwrap() < 177} {
-                let mut paralyzed_turns: u8 = 0;
-                for _ in 0..231 {
-                    if rng.gen_range(0..4 as u8) == 0 {
-                        paralyzed_turns += 1;
-                    }
-                }
-                update_most_paralyzed_turns(most_paralyzed_turns.lock().unwrap(), paralyzed_turns)
-            }
-        });
-
+    for i in 0..thread_count.into() {
+        let battles_to_run = battles / (thread_count.get() - i) as u32;
+        let thread: JoinHandle<(u8, u32)> = thread::spawn(move || run_simulations(battles_to_run));
         threads.push(thread);
+        battles -= battles_to_run;
     }
 
+    let mut most_paralyzed_turns = 0;
+    let mut played_battles = 0;
     for thread in threads {
-        thread.join().unwrap();
+        let (most_paralyzed_thread, played_battles_thread) = thread.join().unwrap();
+        played_battles += played_battles_thread;
+        if most_paralyzed_thread > most_paralyzed_turns {
+            most_paralyzed_turns = most_paralyzed_thread
+        }
     }
 
     let duration: Duration = start_time.elapsed();
 
-    println!("Highest amount of paralyzed turns: {}", *most_paralyzed_turns.lock().unwrap());
-    println!("Number of battles simulated: {}", *played_battles.lock().unwrap());
+    println!("Highest amount of paralyzed turns: {}", most_paralyzed_turns);
+    println!("Number of battles simulated: {}", played_battles);
     println!("The code took: {}s", duration.as_secs_f64());
 }
